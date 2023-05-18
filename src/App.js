741ac478c00786ced8b5c02c167535a8caa37663
hamzaakrammogul <hmughal502@gmail.com>
2023-05-18 14:45:06 +0500

    Completetd useEffect

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
+
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+ 
+ useEffect(()=>{
+  const isUserLoggedIn = localStorage.getItem('isLoggedIn');
+
+  if(isUserLoggedIn === '1'){
+    setIsLoggedIn(true);
+  }
+ }, []);
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
     setIsLoggedIn(true);
+    //key and value pair
+    localStorage.setItem('isLoggedIn', '1');
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn')
     setIsLoggedIn(false);
   };
 
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
@@ -11,20 +11,38 @@ const Login = (props) => {
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
+
+  useEffect(() => {
+    const identifier =setTimeout(() => {
+      console.log("checking the validity")
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+
+      );
+    }, 500);
+
+    return () => {
+     
+      console.log("Clean Up "+enteredEmail);
+      clearTimeout(identifier);
+    };
+  }, [enteredEmail, enteredPassword]);
+
+
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
 
-    setFormIsValid(
-      event.target.value.includes('@') && enteredPassword.trim().length > 6
-    );
+    // setFormIsValid(
+    //   event.target.value.includes('@') && enteredPassword.trim().length > 6
+    // );
   };
 
   const passwordChangeHandler = (event) => {
     setEnteredPassword(event.target.value);
 
-    setFormIsValid(
-      event.target.value.trim().length > 6 && enteredEmail.includes('@')
-    );
+    // setFormIsValid(
+    //   event.target.value.trim().length > 6 && enteredEmail.includes('@')
+    // ); 
   };
 
   const validateEmailHandler = () => {
@@ -44,9 +62,8 @@ const Login = (props) => {
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
         <div
-          className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ''
-          }`}
+          className={`${classes.control} ${emailIsValid === false ? classes.invalid : ''
+            }`}
         >
           <label htmlFor="email">E-Mail</label>
           <input
@@ -58,9 +75,8 @@ const Login = (props) => {
           />
         </div>
         <div
-          className={`${classes.control} ${
-            passwordIsValid === false ? classes.invalid : ''
-          }`}
+          className={`${classes.control} ${passwordIsValid === false ? classes.invalid : ''
+            }`}
         >
           <label htmlFor="password">Password</label>
           <input
